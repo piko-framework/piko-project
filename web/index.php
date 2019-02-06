7b1acf13f14f681b0052946581bd5f78c90b1446
ilhooq <contact@sphilip.com>
2019-02-06 19:02:05 +0100

    Remove wrong license and simplify index.php

--- web/index.php
+++ web/index.php
@@ -1,18 +1,15 @@
 <?php
-/**
- * This file is part of Piko framework
- * @copyright  (C) 2017 PHILIP Sylvain. All rights reserved.
- * @license    LGPL-3.0 see LICENSE file
- */
+use piko\Application;
+use piko\Utils;
 
 define('APP_ROOT', dirname(__DIR__));
 
 require(APP_ROOT . '/vendor/autoload.php');
 
 if (file_exists(APP_ROOT . '/.env')) {
-    \piko\Utils::parseEnvFile(APP_ROOT . '/.env');
+    Utils::parseEnvFile(APP_ROOT . '/.env');
 }
 
 $config = require APP_ROOT . '/config.php';
 
-(new \piko\Application($config))->run();
+(new Application($config))->run();
